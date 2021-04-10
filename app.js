2b9f3b23d85458a8bc4e4d60fc2911e003061417
pns00911 <80819203+pns00911@users.noreply.github.com>
2021-04-10 22:17:14 +0530

    Code_updated_by_Pruthvik_Sheth

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
+ /* Code For JavaScript(Website) (Contributed By : Pruthvik Sheth)  */
 
+
+const header = document.querySelector('header');
+
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    header.style.top = "0px";
+  } else {
+    header.style.top = "-120px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+document.addEventListener('scroll',function(){
+    if(document.documentElement.scrollTop > 100 || document.body.scrollTop > 100)
+    {
+      document.getElementById("upButton").style.display = "block";
+    }
+    else
+    {
+      document.getElementById("upButton").style.display = "none";
+    }
+  });
